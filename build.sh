303dfecfdb879f77b154f9e1c1f32ef748f9ae49
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2020-11-11 08:29:33 +0530

    [skip ci]

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ set -eo pipefail
 APP_NAME=$1
 UPDATE_CACHE=""
 
-docker build -f docker/Dockerfile -t $APP_NAME:latest .
+docker build -f docker/Dockerfile -t $APP_NAME:latest \
+--build-arg APPMODE=$APPMODE \
+--build-arg APPENV=$APPENV .
 
 docker create --name app $APP_NAME:latest
 
