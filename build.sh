533560508b6f592810283c6e66a6164914f06349
nkumar-topcoder <33625707+nkumar-topcoder@users.noreply.github.com>
2020-11-11 11:52:24 +0530

    [skip ci]

--- build.sh
+++ build.sh
@@ -7,20 +7,22 @@ docker build -f docker/Dockerfile -t $APP_NAME:latest \
 --build-arg APPMODE=$APPMODE \
 --build-arg APPENV=$APPENV .
 
-docker create --name app $APP_NAME:latest
+docker build -f docker/mockDockerfile -t mock_$APP_NAME:latest .
 
-if [ -d node_modules ]
-then
-  mv package-lock.json old-package-lock.json
-  docker cp app:/$APP_NAME/package-lock.json package-lock.json
-  set +eo pipefail
-  UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
-  set -eo pipefail
-else
-  UPDATE_CACHE=1
-fi
+#docker create --name app $APP_NAME:latest
 
-if [ "$UPDATE_CACHE" == 1 ]
-then
-  docker cp app:/$APP_NAME/node_modules .
-fi
+#if [ -d node_modules ]
+#then
+#  mv package-lock.json old-package-lock.json
+#  docker cp app:/$APP_NAME/package-lock.json package-lock.json
+#  set +eo pipefail
+#  UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
+#  set -eo pipefail
+#else
+#  UPDATE_CACHE=1
+#fi
+
+#if [ "$UPDATE_CACHE" == 1 ]
+#then
+#  docker cp app:/$APP_NAME/node_modules .
+#fi
