7d037b741b7b493b69a44e207422ee02f2b45b68
Maksym Mykhailenko <maxceem@gmail.com>
2021-02-19 09:45:16 +0200

    fix: edit RB page borken

--- src/routes/ResourceBookingForm/utils.js
+++ src/routes/ResourceBookingForm/utils.js
@@ -11,7 +11,7 @@ import {
   FORM_FIELD_TYPE,
 } from "../../constants";
 
-const EDIT_RESOURCEBOOKING_ROWS = [
+const EDIT_RESOURCE_BOOKING_ROWS = [
   { type: FORM_ROW_TYPE.SINGLE, fields: ["handle"] },
   { type: FORM_ROW_TYPE.SINGLE, fields: ["jobTitle"] },
   { type: FORM_ROW_TYPE.GROUP, fields: ["startDate", "endDate"] },
