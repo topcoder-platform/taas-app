43acfdf5a6b4e4bd4311babed102713a570c9d98
Md Mahidul Haque Alvi <mahidulalvi@bonicinitiatives.biz>
2021-10-25 23:24:38 -0500

    Integrated calendar connection functionality with Nylas & TAAS APIs.
    The following changes are made in this commit:
    
    1. NPM packages 'jsonwebtoken' & 'query-string' have been added to facilitate JWT related functionality & parsing url query parameters;
    2. Updated README.md file with new configuration requirements;
    3. Added new method 'connectCalendar' in interviews service;
    4. Added new useEffect in 'src/routes/PositionDetails/components/PositionCandidates' component to detect calendar connection success/failure from the URL, then open ManageAvailability modal and show result toast notifications;
    5. Updated 'src/routes/PositionDetails/components/InterviewPopup' component to pass required props for the 'ConnectCalendar' component;
    6. Made changes to UI in 'src/routes/PositionDetails/components/InterviewPopup/ConnectCalendar' component to integrate backend functionalities;
    5. Added necessary styles for 'src/routes/PositionDetails/components/InterviewPopup/ConnectCalendar' component;

--- src/services/interviews.js
+++ src/services/interviews.js
@@ -1,5 +1,8 @@
+/* global process */
+
 import { axiosInstance as axios } from "./requestInterceptor";
 import config from "../../config";
+import jwt from "jsonwebtoken";
 
 export const getUserSettings = (userId) => {
   return axios.get(`${config.API.V5}/taas/user-meeting-settings/${userId}`);
@@ -9,6 +12,36 @@ export const deleteCalendar = (userId, calendarId) => {
   return axios.delete(
     `${config.API.V5}/taas/user-meeting-settings/${userId}/calendars/${calendarId}`
   );
+};
+
+/**
+ * Connect calendar
+ *
+ * @param {UUID of current user} userId
+ * @param {Url to get redirected to when api finishes saving calendar in UserMeetingSettings record} appRedirectUrl
+ *
+ * @returns Redirects to Nylas server so user can authenticate & connect account in Nylas
+ */
+export const connectCalendar = (userId, appRedirectUrl) => {
+  const apiRedirectUrl = encodeURIComponent(
+    `${config.API.V5}/taas/user-meeting-settings/callback`
+  );
+
+  const state = jwt.sign(
+    {
+      userId,
+      redirectTo: appRedirectUrl,
+    },
+    process.env.NYLAS_CONNECT_CALENDAR_JWT_SECRET,
+    {
+      algorithm: "HS256",
+      expiresIn: 60,
+    }
+  );
+
+  const nylasCalendarConnectionUrl = `https://api.nylas.com/oauth/authorize?client_id=${process.env.NYLAS_CLIENT_ID}&redirect_uri=${apiRedirectUrl}&response_type=code&scopes=calendar&state=${state}`;
+
+  return (window.location.href = nylasCalendarConnectionUrl);
 };
 
 export const confirmInterview = (candidateJobId, data) => {
